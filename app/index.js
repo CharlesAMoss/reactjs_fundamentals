8f8bdd91c910b77db6948d5b8592353260128360
CharlesAMoss <charlesamoss@yahoo.com>
2016-03-04 17:33:55 -0800

    add props example

--- app/index.js
+++ app/index.js
@@ -3,12 +3,33 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-    render: function () {
+var HelloUser = React.createClass({
+    render: function (){
         return (
-            <div>Hello</div>
-        );
+            var name = "Herman";
+            var peeps = ['Erp', 'Derp', 'Lerp']
+            <div>
+                <h2>Hello, {name}</h2>
+                <ShowList names={peeps} />
+            </div>
+        )
     }
 });
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+var ShowList = React.createClass({
+    render: function (){
+        var listItems = this.props.names.map(function(peep){
+            return <li> {peep} </li>;
+        });
+        return (
+            <div>
+                <h3> Peeps </h3>
+                <ul>
+                    {listItems}
+                </ul>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(<HelloUser />, document.getElementById('app'));
